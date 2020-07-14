933bc98732863799bcfe1ea71b615d9be9d28df3
zhangshengtao <1449048006@qq.com>
2020-07-14 14:36:22 +0800

    nest-demo

---
+++ src/components/Tree/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="tree">
+    <li v-for="(item,index) in treedata" :key="index">
+      <div class="ui-draggable ui-draggable-handle ui-droppable">
+        {{item.text}}
+      </div>
+      <my-tree v-if="item.children && item.children.length>0" :treedata="item.children"></my-tree>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "myTree",
+  props:{
+    treedata:{
+      type: [Array],
+      default: () => []
+    }
+  },
+  created(){
+    console.log('treedata：', this.treedata)
+  }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <Tree :treedata="treedata" />
+  </div>
+</template>
+
+
+<script>
+import Tree from '../components/Tree'
+export default {
+  name: "About",
+  components: {
+    Tree,
+  },
+  data(){
+    return {
+      treedata:[
+        {
+          text:1,
+          children:[
+            {text:12},
+            {text:13}
+          ]
+        },
+        {text:3},
+        {text:4}
+      ]
+    }
+  },
+  created(){
+   
+  }
+}
+</script>
+
+
